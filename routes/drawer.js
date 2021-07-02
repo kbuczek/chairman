3a5d814d8cdde99c4de47db8bf7d057d6999b34f
kbuczek <krzysztofbuczek@live.com>
2021-07-02 18:56:14 +0200

    working schedule List

--- routes/drawer.js
+++ routes/drawer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import ScheduleStackNav from "./scheduleStackNav";
 import HomeStackNav from "./homeStackNav";
 import AboutStackNav from "./aboutStackNav";
 
@@ -9,8 +10,9 @@ const Drawer = createDrawerNavigator();
 export default DrawerNav = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Harmonogram" component={HomeStackNav} />
+      <Drawer.Navigator initialRouteName="Harmonogram">
+        <Drawer.Screen name="Harmonogram" component={ScheduleStackNav} />
+        <Drawer.Screen name="Home" component={HomeStackNav} />
         <Drawer.Screen name="About" component={AboutStackNav} />
       </Drawer.Navigator>
     </NavigationContainer>
